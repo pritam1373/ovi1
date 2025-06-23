9bf77b4a1d33d52366b8e51eaed80fbf2814c729
pritam1373 <ovnandy99@gmail.com>
2025-06-23 21:07:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let a = document.getElementById("bt");
+let b = document.getElementById("vi");
+let c = document.getElementById("photo");
+
+a.onclick=function() {
+  b.style.opacity = 1;
+  c.style.opacity = 0;
+}
